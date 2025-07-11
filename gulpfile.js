8f5ce08324bdf124af860769b3a807ffed51e65d
Alexandr-rudic <rudic.alexandr@gmail.com>
2025-07-11 12:51:45 +0300

    added header

--- gulpfile.js
+++ gulpfile.js
@@ -57,7 +57,7 @@ function images() {
 
 
 function styles() {
-  return src('app/scss/style.scss')
+  return src('app/scss/*.scss')
     .pipe(autoprefixer({
       overrideBrowserslist: ['last 10 versions']
     }))
@@ -84,7 +84,7 @@ function watching() {
       baseDir: 'app/'
     }
   });
-  watch(['app/scss/style.scss'], styles)
+  watch(['app/scss/*.scss'], styles)
   watch(['app/images/src'], images)
   watch(['app/images/sprite'], sprites)
   watch(['app/pages/*', 'app/components/*'], pages)
